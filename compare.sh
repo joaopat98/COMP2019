b85a524011669edff36a6f68c56166e781b2a6e3
Guilherme Tavares <guilherme@din-wdei077.dei.uc.pt>
2019-03-04 15:11:32 +0000

    added flag to script and updated dei git

--- compare.sh
+++ compare.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 file1=$1
 file2=$2
+flag=$3
 flex src/gocompiler.l
 clang lex.yy.c
-./a.out -l < $file1 > output
+./a.out $flag < $file1 > output
 diff output $file2
 rm lex.yy.c a.out output
