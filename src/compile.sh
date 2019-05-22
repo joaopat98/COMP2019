720f254e9199b071eb9c2afd2f980a6cb020ef07
Gdtav <guilhermeditavares@gmail.com>
2019-05-22 17:31:49 +0100

    changed compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -3,7 +3,7 @@ file1=$1
 flag=$2
 flex gocompiler.l
 bison -y -d gocompiler.y
-clang -Wall code_gen.c lex.yy.c nodes.c semantics.c sym_types.c symtab.c y.tab.c
+clang -Wall *.c
 ./a.out $flag < $file1 > out.ll
 llc out.ll
 clang out.s
